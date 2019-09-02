21c3439275171c97e8f532ea14cdf6b15c19060c
owais <owaishunter78@gmail.com>
2019-09-03 03:48:27 +0530

    hmm maja aya

--- examples/linearRegEx.cpp
+++ examples/linearRegEx.cpp
@@ -8,14 +8,14 @@ int main()
     LinearRegressor model(1);
     double alpha;
     int epochs;
-    cout<<"alpha and epochs\n";
+    cout<<"alpha and epochs ,choose alpha less than .05 \n";
     cin>>alpha>>epochs;
-    arma::mat X_train(10, 1);
-    arma::mat y_train(10, 1);
-    for(int i=0;i<10;i++)
+    arma::mat X_train(1000, 1);
+    arma::mat y_train(1000, 1);
+    for(int i=0;i<1000;i++)
     {
         X_train(i, 0) = i;
-        y_train(i, 0) = 2 * i;
+        y_train(i, 0) = (2 * i);
     }
     model.train(X_train,y_train,alpha,epochs);
 
--- src/LinearRegression/LinearRegressor.cpp
+++ src/LinearRegression/LinearRegressor.cpp
@@ -2,7 +2,7 @@
 #include "LinearRegressor.hpp"
 
 using namespace std;
-
+//constructor to initialize number of dependent variables
 LinearRegressor::LinearRegressor(int num){
     number_of_variables = num;
     weights = arma::randu<arma::mat>(number_of_variables + 1, 1);
@@ -19,8 +19,9 @@ void LinearRegressor::train(arma::mat X_train, arma::mat y_train, float alpha, i
         x.insert_cols(0, arma::ones<arma::mat>(X_train.n_rows, 1));
         for(int i=1;i<=epochs;i++)
         {   
-            arma::mat delta_weight = x.t() * (predict(X_train)-y_train);
-            weights -= ((alpha * delta_weight)/X_train.n_rows);
-            cout<<"Epoch completed = "<<i<<" Weights "<<weights<<endl;
+             //updating weights using gradient descent method
+            arma::mat delta=(x.t() * (predict(X_train)-y_train));
+            weights -= ((alpha * delta)/X_train.n_rows);
+            cout<<"Epoch completed = "<<i<<weights<< " " << endl;
         }
 }
