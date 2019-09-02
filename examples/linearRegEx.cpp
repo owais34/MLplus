54aeea44426c66dadc65fbb4a306a7d21355fad7
Samuelimza <osamaazmi1999@gmail.com>
2019-09-02 19:23:54 +0530

    example patched

--- examples/linearRegEx.cpp
+++ examples/linearRegEx.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
-#include <LinearRegressor.hpp>
+#include "../src/LinearRegression/LinearRegressor.hpp"
 
 using namespace std;
 
 int main()
 {
-    LinearRegressor model;
-    cout<<model.number_of_variables<<endl;
-    model.setNumberofVariables(10);
-    for(int i=0;i<model.weights.size();i++)
-        cout<<model.weights[i]<<" ";
-    cout<<endl;
+    LinearRegressor model(1);
 
-    vector<vector<float> > X_train;
-    vector<float> y_train;
+    arma::mat X_train(10, 1);
+    arma::mat y_train(10, 1);
     for(int i=0;i<10;i++)
     {
-        float x,y;
-        cin>>x>>y;
-        vector<float> f;f.push_back(x);X_train.push_back(f);y_train.push_back(y);
+        X_train(i, 0) = i;
+        y_train(i, 0) = 2 * i;
     }
-    model.train(X_train,y_train,0.1,10000);
-    cout<<model.number_of_variables<<endl;
+    model.train(X_train,y_train, 0.001, 100);
 
-    vector<float> out=model.predict(X_train);
-    for(int i=0;i<out.size();i++)
-        cout<<out[i]<<endl;
+    arma::mat out=model.predict(X_train);
+    cout<<out<<endl;
     return 0;
 }
