f9a44da143314b34aada79a475c54be247c7815d
owais34 <owaishunter78@gmail.com>
2019-09-18 00:06:21 +0530

    Added probability table

--- src/naive_bayes/naive_bayes.cpp
+++ src/naive_bayes/naive_bayes.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 //constructor to initialize number of dependent variables
-Naive_bayes::Naive_bayes(int num){
-    number_of_variables = num;
-	
-}
+class Naive_bayes{
+public:	
 
-arma::mat Naive_bayes::predict(arma::mat X_predict){
+Naive_bayes::Naive_bayes(int num,vector<int> categorical_cols);
 
-    return regpred;
-}
+arma::mat Naive_bayes::predict(arma::mat X_predict);
 
-void Naive_bayes::train(arma::mat X_train, arma::mat y_train){
-        
+void Naive_bayes::train(arma::mat X_train, arma::mat y_train);
+
+};
+class Prob_table
+{
+	public:
+		int rows,int cols;
+		arma::mat table;
+		Prob_table(int rows,int cols);
+		
+		void view();
 }
--- src/naive_bayes/naive_bayes.h
+++ src/naive_bayes/naive_bayes.h
@@ -2,13 +2,15 @@
 #include <armadillo>
 
 using namespace std;
+
 class Naive_bayes{
     public:
     int number_of_variables;
+	vector<int> categorical_cols;
+	vector<int> cont_cols;
+    Naive_bayes(int num,vector<int> cat_cols);
 
-    Naive_bayes(int num);
-
-    void train(arma::mat X_train, arma::mat y_train,);
+    void train(arma::mat X_train, arma::mat y_train);
 
     arma::mat predict(arma::mat X_predict);
 };
